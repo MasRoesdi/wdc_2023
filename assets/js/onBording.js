b9d6346b98a2672456d0fe4f9848db9d1d40bb5e
Fadhlu Ibnu <f.ibnu.a1@gmail.com>
2023-06-03 10:33:33 +0700

    hehe td splash screen masih nonaktif

--- assets/js/onBording.js
+++ assets/js/onBording.js
@@ -12,46 +12,46 @@
 //     element.style.transform = 'translateY(-100vh)'
 // }
 
-// document.addEventListener("DOMContentLoaded", function (event) {
-//     document.getElementById('onBording').style.backgroundImage = 'url(../../assets/images/on_boarding_background.jpg)';
+document.addEventListener("DOMContentLoaded", function (event) {
+    document.getElementById('onBording').style.backgroundImage = 'url(../../assets/images/on_boarding_background.jpg)';
 
-//     const background_peta_indonesia = document.getElementById('background_peta_indonesia')
-//     background_peta_indonesia.style.backgroundImage = 'url(../../assets/images/background_peta_indonesia.png)'
+    const background_peta_indonesia = document.getElementById('background_peta_indonesia')
+    background_peta_indonesia.style.backgroundImage = 'url(../../assets/images/background_peta_indonesia.png)'
 
-//     setTimeout(function () {
-//         background_peta_indonesia.style.opacity = '1'
-//     }, 2000)
+    setTimeout(function () {
+        background_peta_indonesia.style.opacity = '1'
+    }, 2000)
 
-//     var textP = "EXPLORE THE WONDERFULL OF";
-//     var speedP = 100; // Kecepatan pengetikan dalam milidetik
-//     var iP = 0;
+    var textP = "EXPLORE THE WONDERFULL OF";
+    var speedP = 100; // Kecepatan pengetikan dalam milidetik
+    var iP = 0;
 
-//     var textH = "INDONESIA";
-//     var speedH = 100; // Kecepatan pengetikan dalam milidetik
-//     var iH = 0;
+    var textH = "INDONESIA";
+    var speedH = 100; // Kecepatan pengetikan dalam milidetik
+    var iH = 0;
 
-//     function typeWriterP() {
-//         if (iP < textP.length) {
-//             document.getElementById("pOnBoarding").innerHTML += textP.charAt(iP);
-//             iP++;
-//             setTimeout(typeWriterP, speedP);
-//         }
-//     }
-//     function typeWriterH() {
-//         if (iH < textH.length) {
-//             document.getElementById("hOnBoarding").innerHTML += textH.charAt(iH);
-//             iH++;
-//             setTimeout(typeWriterH, speedH);
-//         }
-//     }
-//     setTimeout(typeWriterP, 3000)
-//     setTimeout(typeWriterH, 6000)
+    function typeWriterP() {
+        if (iP < textP.length) {
+            document.getElementById("pOnBoarding").innerHTML += textP.charAt(iP);
+            iP++;
+            setTimeout(typeWriterP, speedP);
+        }
+    }
+    function typeWriterH() {
+        if (iH < textH.length) {
+            document.getElementById("hOnBoarding").innerHTML += textH.charAt(iH);
+            iH++;
+            setTimeout(typeWriterH, speedH);
+        }
+    }
+    setTimeout(typeWriterP, 3000)
+    setTimeout(typeWriterH, 6000)
 
-//     setTimeout(function () {
-//         const element = document.getElementById('onBording')
-//         const body = document.getElementsByTagName('body')
+    setTimeout(function () {
+        const element = document.getElementById('onBording')
+        const body = document.getElementsByTagName('body')
 
-//         body[0].classList.remove('overflow-hidden')
-//         element.style.transform = `translateY(-${window.screen.availHeight}vh)`
-//     }, 10000)
-// })
+        body[0].classList.remove('overflow-hidden')
+        element.style.transform = `translateY(-${window.screen.availHeight}vh)`
+    }, 10000)
+})

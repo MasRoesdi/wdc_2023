623c58e1b35c33dc5c9261d9bacdb994e1c02a5f
Fadhlu Ibnu <f.ibnu.a1@gmail.com>
2023-06-06 08:20:30 +0700

    payment selection feature

--- assets/js/payment-method.js
+++ assets/js/payment-method.js
@@ -1,2 +1,28 @@
 const selectPaymentMethod = document.getElementById('select-payment-method').children
-console.log(selectPaymentMethod)
+let idArr = [];
+for (var i = 0; i < selectPaymentMethod.length; i++) {
+    if (selectPaymentMethod[i].id != '') {
+        idArr.push(selectPaymentMethod[i].id);
+    }
+}
+
+function selectMethod(id, selectMethod){
+    for (let index = 0; index < idArr.length; index++) {
+        const theId = idArr[index];
+        let element = document.getElementById(theId)
+        if (id == theId) {
+            element.classList.add('bg-[#4F94D74D]')
+            element.classList.remove('bg-transparent')
+            document.getElementById('displaySelectedMethod').innerHTML = `Virtual Account - ${selectMethod}` 
+        }else{
+            element.classList.add('bg-transparent')
+            element.classList.remove('bg-[#4F94D74D]')
+        }
+        // if (id != theId) {
+        //     element.classList.add('bg-transparent')
+        // }else{
+        //     element.classList.add('bg-[#4F94D74D]')
+        // }
+
+    }
+}
